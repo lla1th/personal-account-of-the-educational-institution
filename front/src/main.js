a154f8347b43446fc3511ca69253d742c3bb4076
Samokhin M <grossewirbelwind@gmail.com>
2023-03-08 21:04:10 +0300

    fix adding components from project

--- front/src/main.js
+++ front/src/main.js
@@ -1,16 +1,10 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { Quasar } from 'quasar';
 import App from './App.vue';
 
 /** router path */
-import router from './router/routers.js';
-
-/** Pinia install */
-import { createPinia } from 'pinia';
-
-const pinia = createPinia();
-
-/** Quasar install */
-import { Quasar } from 'quasar';
+import router from './router/routers';
 
 /** Import css Quasar */
 import 'quasar/src/css/index.sass';
@@ -19,17 +13,31 @@ import 'quasar/src/css/index.sass';
 import '@quasar/extras/material-icons/material-icons.css';
 import '@quasar/extras/material-icons-round/material-icons-round.css';
 
+/** import components */
+import NightDrawer from './components/high/NightDrawer.vue';
+import NightCard from './components/ui/NightCard.vue';
+import NightTitle from './components/high/NightTitle.vue';
+
+const pinia = createPinia();
+
+const components = [NightTitle, NightCard, NightDrawer];
+
 const quasarSettings = { plugins: {} };
 
-/** import components (beta) */
-//todo перенести, доработать через цикл
-import NightDrawer from "./components/high/NightDrawer.vue";
-import NightCard from "./components/ui/NightCard.vue";
+const initApp = async () => {
+  const app = createApp(App);
+  components.forEach((element) => {
+    // eslint-disable-next-line no-underscore-dangle
+    const nameComponent = element.__name;
 
-createApp(App)
-    .use(router)
+    app.component(nameComponent, element);
+  });
+
+  app.use(router)
     .use(pinia)
-    .use(Quasar, quasarSettings)
-    .component('NightDrawer', NightDrawer)
-    .component('NightCard', NightCard)
-    .mount('#app')
+    .use(Quasar, quasarSettings);
+
+  app.mount('#app');
+};
+
+initApp();
