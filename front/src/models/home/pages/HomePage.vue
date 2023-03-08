090bce2e5b65ea1000f10a4ceff608d448e2539b
Samokhin M <grossewirbelwind@gmail.com>
2023-03-08 23:02:47 +0300

    update content home page

--- front/src/models/home/pages/HomePage.vue
+++ front/src/models/home/pages/HomePage.vue
@@ -13,21 +13,21 @@ const routerPage = (url) => {
 </script>
 
 <template>
-  <q-page
-    padding
-    class="q-pa-md row items-start content-start q-gutter-lg"
-  >
-    <NightCard
-      v-for="(item, index) in store.cards"
-      :key="`menu-card-${index}`"
-      :title="item.title"
-      @click="routerPage(item.url)"
-    >
-      <template #sub-title>
-        <div class="text-subtitle2">
-          {{ item.subTitle }}
-        </div>
-      </template>
-    </NightCard>
+  <q-page padding>
+    <NightTitle title="Главная" />
+    <div class="q-pa-md row items-start content-start q-gutter-lg">
+      <NightCard
+        v-for="(item, index) in store.cards"
+        :key="`menu-card-${index}`"
+        :title="item.title"
+        @click="routerPage(item.url)"
+      >
+        <template #sub-title>
+          <div class="text-subtitle2">
+            {{ item.subTitle }}
+          </div>
+        </template>
+      </NightCard>
+    </div>
   </q-page>
 </template>
